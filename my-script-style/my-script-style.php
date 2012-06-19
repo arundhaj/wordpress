c1a9451173cdfd65c5bb02e4afeddea706a631b3
arundhaj <arundhaj@gmail.com>
2012-06-19 14:37:50 +0530

    updated script.

---
+++ my-script-style/my-script-style.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: my-script-style
+Plugin URI: http://www.arundhaj.com
+Description: To include your own custom css and script file that are independent on theme.
+Author: Arunkumar Dharuman
+Author URI: http://www.arundhaj.com
+Version: 1.0
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. if you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+
+*/
+
+if (!defined("WP_CONTENT_URL")) define("WP_CONTENT_URL", get_option("siteurl") . "/wp-content");
+if (!defined("WP_PLUGIN_URL"))  define("WP_PLUGIN_URL",  WP_CONTENT_URL        . "/plugins");
+
+function my_script_style() {
+	$path = WP_PLUGIN_URL . '/my-script-style/';
+	$url = $path . '/wp_mystyle.css';
+
+	echo "\n\n<!-- your plugin css styles -->\n";
+    echo "<link rel='stylesheet' type='text/css' href='$url'>\n";
+    echo "<!-- /your plugin css styles -->\n\n";
+}
+
+add_action('wp_head', 'my_script_style');
+?>
